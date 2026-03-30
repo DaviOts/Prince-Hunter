2ec3dd9ec1673139fe472806c1a2876d57c64b66
otavs <daviotaviano1@gmail.com>
2026-03-30 12:12:14 -0300

    feat(auth): add HttpOnly secure cookies for refresh token on auth routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,22 +5,52 @@ import { LoginDto } from './dto/login.dto';
 import { JwtAuthGuard } from './dto/guards/jwt-auth.guard';
 import { CurrentUser } from './dto/guards/current-user.decorator';
 import type { JwtPayload } from './interface/jwt-payload.interface';
+import { Res } from '@nestjs/common';
+import type { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  async register(@Body() createUserDto: RegisterDto) {
-    return this.authService.register(
+  async register(
+    @Body() createUserDto: RegisterDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { access_token, refresh_token } = await this.authService.register(
       createUserDto.email,
       createUserDto.password,
     );
+
+    res.cookie('refresh_token', refresh_token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return { access_token };
   }
 
   @Post('login')
-  async login(@Body() loginDto: LoginDto) {
-    return this.authService.login(loginDto.email, loginDto.password);
+  async login(
+    @Body() loginDto: LoginDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { access_token, refresh_token } = await this.authService.login(
+      loginDto.email,
+      loginDto.password,
+    );
+
+    //set refreshtoken like cookie HTTPS-only
+    res.cookie('refresh_token', refresh_token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return { access_token };
   }
 
   @Get('me')
