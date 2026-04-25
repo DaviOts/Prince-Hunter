255b9b0823e069ff1ef2a3a9fa205d0c5e87f7db
otavs <daviotaviano1@gmail.com>
2026-04-25 15:23:56 -0300

    test(watchlist): implement unit tests and mock dependencies

--- src/modules/watchlist/watchlist.controller.spec.ts
+++ src/modules/watchlist/watchlist.controller.spec.ts
@@ -1,14 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WatchlistController } from './watchlist.controller';
 import { WatchlistService } from './watchlist.service';
+import { WatchlistStatus } from '@prisma/client';
 
 describe('WatchlistController', () => {
   let controller: WatchlistController;
 
+  const mockWatchlistService = {
+    addGameToWatchlist: jest.fn(),
+    removeGameFromWatchlist: jest.fn(),
+    getWatchlist: jest.fn(),
+    updateWatchlist: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [WatchlistController],
-      providers: [WatchlistService],
+      providers: [
+        {
+          provide: WatchlistService,
+          useValue: mockWatchlistService,
+        },
+      ],
     }).compile();
 
     controller = module.get<WatchlistController>(WatchlistController);
@@ -16,5 +29,53 @@ describe('WatchlistController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should add game to watchlist', async () => {
+    const gameSlug = 'elden-ring';
+    const user = { sub: 'user-1', email: 'a@a.com', iat: 0, exp: 0 };
+    mockWatchlistService.addGameToWatchlist.mockReturnValue({});
+    await controller.gameSlug(gameSlug, user);
+    expect(mockWatchlistService.addGameToWatchlist).toHaveBeenCalledWith(
+      gameSlug,
+      user.sub,
+    );
+  });
+
+  it('should get watchlist', async () => {
+    const user = { sub: 'user-1', email: 'a@a.com', iat: 0, exp: 0 };
+    mockWatchlistService.getWatchlist.mockReturnValue({});
+    await controller.getWatchlist(user);
+    expect(mockWatchlistService.getWatchlist).toHaveBeenCalledWith(user.sub);
+  });
+
+  it('should update watchlist', async () => {
+    const gameSlug = 'elden-ring';
+    const user = { sub: 'user-1', email: 'a@a.com', iat: 0, exp: 0 };
+    const dto = {
+      status: WatchlistStatus.PLAYING,
+      rating: 10,
+      review: 'WOWW badass',
+    };
+    mockWatchlistService.updateWatchlist.mockReturnValue({});
+    await controller.updateWatchlist(gameSlug, dto, user);
+    expect(mockWatchlistService.updateWatchlist).toHaveBeenCalledWith(
+      gameSlug,
+      user.sub,
+      dto.status,
+      dto.rating,
+      dto.review,
+    );
+  });
+
+  it('should remove game from watchlist', async () => {
+    const gameSlug = 'elden-ring';
+    const user = { sub: 'user-1', email: 'a@a.com', iat: 0, exp: 0 };
+    mockWatchlistService.removeGameFromWatchlist.mockReturnValue({});
+    await controller.removeGame(gameSlug, user);
+    expect(mockWatchlistService.removeGameFromWatchlist).toHaveBeenCalledWith(
+      gameSlug,
+      user.sub,
+    );
   });
 });
--- src/modules/watchlist/watchlist.service.spec.ts
+++ src/modules/watchlist/watchlist.service.spec.ts
@@ -1,12 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WatchlistService } from './watchlist.service';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+import { Prisma, WatchlistStatus } from '@prisma/client';
+import { ConflictException } from '@nestjs/common';
 
 describe('WatchlistService', () => {
   let service: WatchlistService;
 
+  const mockPrisma = {
+    game: {
+      findUnique: jest.fn(),
+    },
+    watchlist: {
+      create: jest.fn(),
+      findMany: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+    },
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WatchlistService],
+      providers: [
+        WatchlistService,
+        {
+          provide: PrismaService,
+          useValue: mockPrisma,
+        },
+      ],
     }).compile();
 
     service = module.get<WatchlistService>(WatchlistService);
@@ -14,5 +35,90 @@ describe('WatchlistService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should add game to watchlist', async () => {
+    const gameSlug = 'elden-ring';
+    const userId = 'user-1';
+    const watchlist = { id: 'w1', gameSlug, userId };
+    mockPrisma.watchlist.create.mockResolvedValue(watchlist);
+    mockPrisma.game.findUnique.mockResolvedValue({
+      id: 'g1',
+      slug: 'elden-ring',
+    });
+    const result = await service.addGameToWatchlist(gameSlug, userId);
+    expect(mockPrisma.watchlist.create).toHaveBeenCalledWith({
+      data: { gameSlug, userId },
+    });
+    expect(result).toEqual(watchlist);
+  });
+
+  it('should throw ConflictException when game is already in watchlist', async () => {
+    const gameSlug = 'elden-ring';
+    const userId = 'user-1';
+    const error = new Prisma.PrismaClientKnownRequestError(
+      'Unique constraint violation',
+      {
+        code: 'P2002',
+        clientVersion: '0.0.0',
+      },
+    );
+    mockPrisma.game.findUnique.mockResolvedValue({
+      id: 'g1',
+      slug: 'elden-ring',
+    });
+    mockPrisma.watchlist.create.mockRejectedValue(error);
+    await expect(service.addGameToWatchlist(gameSlug, userId)).rejects.toThrow(
+      ConflictException,
+    );
+  });
+
+  it('should update game in watchlist', async () => {
+    const gameSlug = 'elden-ring';
+    const userId = 'user-1';
+    const dto = {
+      status: WatchlistStatus.PLAYING,
+      rating: 5,
+      review: 'amazinggggggg bro',
+    };
+    const updatedWatchlist = {
+      id: 'w1',
+      gameSlug,
+      userId,
+      status: WatchlistStatus.PLAYING,
+    };
+    mockPrisma.watchlist.update.mockResolvedValue(updatedWatchlist);
+    const result = await service.updateWatchlist(
+      gameSlug,
+      userId,
+      dto.status,
+      dto.rating,
+      dto.review,
+    );
+    expect(mockPrisma.watchlist.update).toHaveBeenCalledWith({
+      where: { userId_gameSlug: { userId, gameSlug } },
+      data: {
+        status: dto.status,
+        rating: dto.rating,
+        review: dto.review,
+      },
+    });
+    expect(result).toEqual(updatedWatchlist);
+  });
+
+  it('should remove game from watchlist', async () => {
+    const gameSlug = 'elden-ring';
+    const userId = 'user-1';
+    const watchlist = { id: 'w1', gameSlug, userId };
+    mockPrisma.game.findUnique.mockResolvedValue({
+      id: 'g1',
+      slug: 'elden-ring',
+    });
+    mockPrisma.watchlist.delete.mockResolvedValue(watchlist);
+    const result = await service.removeGameFromWatchlist(gameSlug, userId);
+    expect(mockPrisma.watchlist.delete).toHaveBeenCalledWith({
+      where: { userId_gameSlug: { userId, gameSlug } },
+    });
+    expect(result).toEqual(watchlist);
   });
 });
